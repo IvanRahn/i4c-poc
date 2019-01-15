ae4828b9ba6cbabeae5d152ba34a44c7b2df84be
Ivan <ivan.rahn@gmail.com>
2019-01-15 14:21:20 +1100

    add analytics

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, lazy } from 'react';
 import { connect } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ButtonLink from "./modules/ButtonLink";
-
 
 class App extends Component {
   render() {
@@ -11,8 +10,8 @@ class App extends Component {
       <BrowserRouter>
         <>
 
-        <ButtonLink href="#" text={"link"}/>
-        <ButtonLink href="google.com" color={"green"} text="link2"/>
+        <ButtonLink href="#" text={"link"} location={"Navbar"} {...this.props}/>
+        <ButtonLink href="www.google.com" color={"green"} text="link2"/>
         <ButtonLink href="react.com" color={"red"} text="link3"/>
 
         </>
@@ -22,5 +21,10 @@ class App extends Component {
       );
   }
 }
+// const ReactGA = lazy(() => import("react-ga"));
+import("react-ga").then(ReactGA => {
+  ReactGA.initialize('UA-132415809-3')
+});
+
 
 export default connect()(App);
--- src/components/modules/ButtonLink.js
+++ src/components/modules/ButtonLink.js
@@ -1,6 +1,7 @@
-import React, {Component} from "react";
+import React, {Component, lazy} from "react";
 import {Link} from "react-router-dom";
 import styled from "styled-components";
+const ReactGA = lazy(() => import('react-ga'));
 
 const ButtonLink = styled(Link)`
     border-bottom: 3px solid ${props => props.color || "black"};
@@ -19,11 +20,20 @@ const ButtonLink = styled(Link)`
 
 
 class StyledLink extends Component {
+    analytics = () => {
+        
+        const {location, text} = this.props;
+        ReactGA.event({
+            category: 'ButtonLink',
+            action: `Location: ${location}, Button:${text}`,
+          });
+    }
     render() {
+        console.log(this.props)
         const {color, text, href} = this.props
         // const {ButtonText} = this.props; 
         return (
-        <ButtonLink to={`${href}`} color={color}>{text}</ButtonLink>
+        <ButtonLink to={`${href}`} color={color} onClick={this.analytics}>{text}</ButtonLink>
         )
     }
 }   
