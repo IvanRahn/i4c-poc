bf52015f95d457d4ae172f421f955b33c61735e5
Mike McCallum <mikemgmcc@gmail.com>
2019-01-15 13:58:31 +1100

    implemented card headers and card text

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Button from "./modules/button";
+import HowItWorksCard from "./modules/HowItWorksCard";
 
 
 class App extends Component {
   render() {
     return (
+      
       <BrowserRouter>
+        <>
+        {/* <Button/> */}
 
-        <section>Hello</section>
-        <Button/>
+        <HowItWorksCard 
+        cardHeading = "You choose your cause" 
+        cardText = "With over 50,000 charities in Australia, how do you make the right choice? Our team of analysts determine the best charities that serve the cause of your choice."
+        />
+        
+        </>
       </BrowserRouter>
       );
   }
--- src/components/modules/button.js
+++ src/components/modules/button.js
@@ -1,20 +1,20 @@
-import React, {Component} from "react";
-import Link from "react-router-dom";
-import styled from "styled-components";
+// import React, {Component} from "react";
+// import Link from "react-router-dom";
+// import styled from "styled-components";
 
-const StyledButton = styled.button`
+// const StyledButton = styled.button`
 
-`
+// `
 
 
-class Button extends Component {
-    render() {
-        return (
-        <Link to="/">
-        <StyledButton />
-        </Link>
-        )
-    }
-}   
-export default Button;
+// class Button extends Component {
+//     render() {
+//         return (
+//         <Link to="/">
+//         <StyledButton />
+//         </Link>
+//         )
+//     }
+// }   
+// export default Button;
 
