3ca9a0f5355fc5583c529d21b17966ac9788f5c1
Ivan <ivan.rahn@gmail.com>
2019-01-16 10:05:48 +1100

    display components

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,16 @@
-import React, { Component, Fragment, lazy } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import MoreI4cCard from "./modules/moreI4cCard";
-import ButtonLink from "./modules/ButtonLink";
-import HowItWorksCard from "./modules/HowItWorksCard";
-import image from "./../img/cartoon.jpg"
+import HomePage from "./pages/HomePage";
 class App extends Component {
-
   render() {
     return (
-
-       <section>
-
-        <BrowserRouter>
-        <>
-        
-        <HowItWorksCard 
-        cardHeading = "You choose your cause" 
-        cardText = {
-        <div>
-          <p> With over 50,000 charities in Australia, how do you make the right choice? </p> 
-          <p> Our team of analysts determine the best charities that serve the cause of your choice. </p>
-        </div>
-        }
-        CardImage = {image}
-        />
-
-        <MoreI4cCard CardText = "This is some text" CardHeading = "This is a heading" CardImage = {image} />
-
-        <ButtonLink href="#" text={"link"} location={"Navbar"} />
-        <ButtonLink href="www.google.com" color={"green"} text="link2"/>
-        <ButtonLink href="react.com" color={"red"} text="link3"/>
-
-        </>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
       </BrowserRouter>
-      </section>
-
       );
   }
 }
-// const ReactGA = lazy(() => import("react-ga"));
-import("react-ga").then(ReactGA => {
-  ReactGA.initialize('UA-132415809-3')
-});
-
-
 export default connect()(App);
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import MoreI4cCard from "./../modules/MoreI4cCard";
 import ButtonLink from "./../modules/ButtonLink";
+import HowItWorksCard from "./modules/HowItWorksCard";
+import image from "./../img/cartoon.jpg"
 
 class HomePage extends Component {
     
@@ -12,7 +14,16 @@ class HomePage extends Component {
                 <h1>Welcome To HomePage</h1>
                 <BrowserRouter>
                     <>
-                    
+                    <HowItWorksCard 
+        cardHeading = "You choose your cause" 
+        cardText = {
+        <div>
+          <p> With over 50,000 charities in Australia, how do you make the right choice? </p> 
+          <p> Our team of analysts determine the best charities that serve the cause of your choice. </p>
+        </div>
+        }
+        CardImage = {image}
+        />
                     <MoreI4cCard  CardText = "This is some text" CardHeading = "This is a heading" CardImage = "http://writingexercises.co.uk/images2/randomimage/pipe-sculpture.jpg" />
                     <ButtonLink href="#" text={"link"} {...this.props} section={"The only section"}/>
                     <ButtonLink href="google.com" color={"green"} text="link2"/>
