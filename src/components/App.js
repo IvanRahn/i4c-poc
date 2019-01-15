67efec8d3ec21c5eb2e3ee4d9e3eff4406b259db
Ivan <ivan.rahn@gmail.com>
2019-01-15 14:35:08 +1100

    anayltics

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import { connect } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import MoreI4cCard from "./modules/moreI4cCard";
 import ButtonLink from "./modules/ButtonLink";
-
+import HowItWorksCard from "./modules/HowItWorksCard";
+import image from "./../img/cartoon.jpg"
 class App extends Component {
 
   render() {
@@ -17,9 +18,9 @@ class App extends Component {
         cardHeading = "You choose your cause" 
         cardText = "With over 50,000 charities in Australia, how do you make the right choice? Our team of analysts determine the best charities that serve the cause of your choice."
         />
-        <MoreI4cCard CardText = "This is some text" CardHeading = "This is a heading" CardImage = "http://writingexercises.co.uk/images2/randomimage/pipe-sculpture.jpg" />
+        <MoreI4cCard CardText = "This is some text" CardHeading = "This is a heading" CardImage = {image} />
 
-        <ButtonLink href="#" text={"link"} location={"Navbar"} {...this.props}/>
+        <ButtonLink href="#" text={"link"} location={"Navbar"} />
         <ButtonLink href="www.google.com" color={"green"} text="link2"/>
         <ButtonLink href="react.com" color={"red"} text="link3"/>
 
--- src/components/modules/ButtonLink.js
+++ src/components/modules/ButtonLink.js
@@ -1,7 +1,7 @@
 import React, {Component, lazy} from "react";
 import {Link} from "react-router-dom";
 import styled from "styled-components";
-const ReactGA = lazy(() => import('react-ga'));
+import ReactGA from "react-ga";
 
 const ButtonLink = styled(Link)`
     border-bottom: 3px solid ${props => props.color || "black"};
@@ -22,16 +22,15 @@ const ButtonLink = styled(Link)`
 class StyledLink extends Component {
     analytics = () => {
         
-        const {location, text} = this.props;
+        const {section, text, location} = this.props;
         ReactGA.event({
             category: 'ButtonLink',
-            action: `Location: ${location}, Button:${text}`,
+            action: `Location: ${section}, Button:${text}, Page: ${location.pathname}`,
           });
     }
     render() {
         console.log(this.props)
         const {color, text, href} = this.props
-        // const {ButtonText} = this.props; 
         return (
         <ButtonLink to={`${href}`} color={color} onClick={this.analytics}>{text}</ButtonLink>
         )
