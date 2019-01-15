0af9b7a8296644a53d29fb31a9c80e65b1f9cede
Ivan <ivan.rahn@gmail.com>
2019-01-16 10:20:43 +1100

    move Homepage.js

--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 import MoreI4cCard from "../../modules/MoreI4cCard";
 import ButtonLink from "../../modules/ButtonLink";
 import ImpactSection from './ImpactSection';
+import HowItWorksCard from './../../modules/HowItWorksCard'
+import image from './../../../img/cartoon.jpg'
 class HomePage extends Component {
     
     render() {
@@ -11,9 +13,19 @@ class HomePage extends Component {
             <section>
                 <h1>Welcome To HomePage</h1>
                 <ImpactSection />
+                <HowItWorksCard
+        cardHeading = "You choose your cause" 
+        cardText = {
+        <div>
+          <p> With over 50,000 charities in Australia, how do you make the right choice? </p> 
+          <p> Our team of analysts determine the best charities that serve the cause of your choice. </p>
+        </div>
+        }
+        CardImage = {image}
+        />
                 <BrowserRouter>
                     <>
-                    <MoreI4cCard CardText = "This is some text" CardHeading = "This is a heading" CardImage = "http://writingexercises.co.uk/images2/randomimage/pipe-sculpture.jpg" />
+                    <MoreI4cCard CardText = "This is some text" CardHeading = "This is a heading" CardImage = {image}/>
                     <ButtonLink href="#" text={"link"} {...this.props} section={"The only section"}/>
                     <ButtonLink href="google.com" color={"green"} text="link2"/>
                     <ButtonLink href="react.com" color={"red"} text="link3"/>
