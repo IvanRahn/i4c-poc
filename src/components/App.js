36f7253623ff4dea9be3b1638becf8d50d01dcc1
Mike McCallum <mikemgmcc@gmail.com>
2019-01-15 15:55:00 +1100

    HowItWorksCard works and implemented basic style layout

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,15 @@ import React, { Component, Fragment } from 'react';
 import { connect } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import MoreI4cCard from "./modules/moreI4cCard";
-import ButtonLink from "./modules/ButtonLink";
+import ButtonLink from "./modules/ButtonLink"; 
+import HowItWorksCard from "./modules/HowItWorksCard";
 
 
 class App extends Component {
 
   render() {
     return (
-      
+
        <section>
 
         <BrowserRouter>
@@ -17,6 +18,8 @@ class App extends Component {
         <HowItWorksCard 
         cardHeading = "You choose your cause" 
         cardText = "With over 50,000 charities in Australia, how do you make the right choice? Our team of analysts determine the best charities that serve the cause of your choice."
+        CardImage =
+         "https://previews.123rf.com/images/canbedone/canbedone1602/canbedone160200129/52566812-happy-cartoon-businessman-standing-and-exulting-isolated.jpg"
         />
         <MoreI4cCard CardText = "This is some text" CardHeading = "This is a heading" CardImage = "http://writingexercises.co.uk/images2/randomimage/pipe-sculpture.jpg" />
 
--- src/components/modules/HowItWorksCard.js
+++ src/components/modules/HowItWorksCard.js
@@ -1,26 +1,86 @@
 import React, { Component } from "react";  
 import styled from "styled-components";
 
+const CardWrapper = styled.div`
+overflow: hidden;
+padding: 0 0 32px;
+margin: 48px auto 0;
+width: 400px;
+border-style: solid;
+border-radius: 5px;
+background-color: green;
+color: white;
+`
+const OrdList = styled.div`
+padding: 2px 16px;
+ol {
+  max-width: 350px;
+  counter-reset: my-awesome-counter;
+  list-style: none;
+  padding-left: 40px;
+}
+ol li {
+  margin: 0 0 0.5rem 0;
+  counter-increment: my-awesome-counter;
+  position: relative;
+}
+ol li::before {
+    content: counter(my-awesome-counter);
+    color: white;
+    font-size: 2.5rem;
+    font-weight: bold;
+    position: absolute;
+    --size: 50px;
+    left: calc(-1 * var(--size) - 10px);
+    line-height: var(--size);
+    width: var(--size);
+    height: var(--size);
+    top: 0;
+    background: transparent;
+    border-radius: 50%;
+    text-align: center;
+    border-style: solid;
+}
 
+body {
+  background: #191919;
+  color: #eee;
+  font-family: Lato, sans-serif;
+  line-height: 1.4;
+  font-size: 90%;
+  margin: 2rem;
+}
+`
+
+const ImageContainer = styled.img`
+    display: flex;
+    flex-direction: column;
+    height: 200px;
+    width: 200px;
+    border-radius: 100%;
+    margin: auto;
+`
 
 
 class HowitWorksCard extends Component {
 
     render() { 
-        const {cardHeading, cardText } = this.props
+        const {cardHeading, cardText, CardImage } = this.props
         
         return (
             <div>
-                <div className = "card">
-                    <img href="/" alt= "avatar" src= "" />
-                </div>   
-                <ol className = "content">
-                    <li>
-                        <h1>{cardHeading}</h1> 
-                        <h3>{cardText}</h3>
-                        <h3>{cardText}</h3>
-                    </li>
-                </ol>  
+                <CardWrapper>
+                <ImageContainer src= {CardImage} />
+                    <OrdList>
+                        <ol>
+                            <li>
+                                <h1>{cardHeading}</h1> 
+                                <h3>{cardText}</h3>
+                                <h3>{cardText}</h3>
+                            </li>
+                        </ol>
+                    </OrdList>  
+                </CardWrapper>   
             </div>  
         );
     }
