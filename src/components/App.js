ed23233b2a559b8bb9ab96626539d9d49b264989
David Ramsdale <davidramsdale1991@gmail.com>
2019-01-15 11:14:00 +1100

    add more_i4c_card to modules

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import More_i4c_card from './modules/More_i4c_card';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
 
-        <section>Hello</section>
+        <More_i4c_card />
         
       </BrowserRouter>
       );
---
+++ src/components/modules/More_i4c_card.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class More_i4c_card extends Component {
+    
+    render(){
+        return(
+            <div>
+                This is a card
+            </div>
+        )
+    }
+}
+
+export default More_i4c_card;
