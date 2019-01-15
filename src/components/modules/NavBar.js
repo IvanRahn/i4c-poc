681af3b31f48a57be97a9e33db1c755d8b74d43d
David Ramsdale <davidramsdale1991@gmail.com>
2019-01-16 09:53:12 +1100

    Add styling to navbar

--- src/components/modules/NavBar.js
+++ src/components/modules/NavBar.js
@@ -5,8 +5,16 @@ import styled from "styled-components";
 const NavbarStyling = styled.nav`
     position: sticky;
     display: flex;
+    border-bottom: 1px solid grey;
+    padding: 32px;
 `
-
+const LogoContainer = styled.img`
+    height: 50px; 
+    width: 50px;
+`
+const DivStyling = styled.div`
+    display: flex;
+`
 const UnorderedListStyling = styled.ul`
     list-style-type: none;
     margin: 0;
@@ -19,16 +27,25 @@ class NavBar extends Component {
     render() {
         return(
             <NavbarStyling>
-                <p>LOGO</p>
-                <UnorderedListStyling>    
-                    <li> <ButtonLink text="ABOUT US"/> </li>
-                    <li> <ButtonLink text="HOW IT WORKS"/> </li>
-                    <li> <ButtonLink text="OUR IMPACT"/> </li>
-                    <li> <ButtonLink text="CONTACT US"/> </li>
+
+                <LogoContainer>
+
+                </LogoContainer>
+
+                <DivStyling>
+                    <UnorderedListStyling>    
+                        <li> <ButtonLink text="ABOUT US"/> </li>
+                        <li> <ButtonLink text="HOW IT WORKS"/> </li>
+                        <li> <ButtonLink text="OUR IMPACT"/> </li>
+                        <li> <ButtonLink text="CONTACT US"/> </li>
+                    </UnorderedListStyling>
                     
-                    <li> <ButtonLink text="LOG IN" color={"green"}/> </li>
-                    <li> <ButtonLink text="JOIN" color={"green"}/> </li>
-                </UnorderedListStyling>
+                    <UnorderedListStyling>
+                        <li> <ButtonLink text="LOG IN" color={"green"}/> </li>
+                        <li> <ButtonLink text="JOIN" color={"green"}/> </li>
+                    </UnorderedListStyling>
+                </DivStyling>
+
             </NavbarStyling >
         )
     }
