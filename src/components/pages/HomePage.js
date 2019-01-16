6a16b967560126ccd59ab7090ce750250e9ce594
David Ramsdale <davidramsdale1991@gmail.com>
2019-01-16 11:10:46 +1100

    Add featured cause card

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 import MoreI4cCard from "./../modules/MoreI4cCard";
 import ButtonLink from "./../modules/ButtonLink";
 import HowItWorksCard from "./../modules/HowItWorksCard";
-import image from "./../../img/cartoon.jpg"
+import FeaturedCauseCard from "./../modules/FeaturedCauseCard";
+import image from "./../../img/cartoon.jpg";
 import NavBar from "./../modules/NavBar";
 
 class HomePage extends Component {
@@ -26,16 +27,11 @@ class HomePage extends Component {
         />
                 <BrowserRouter>
                     <>
+                    <FeaturedCauseCard CardText = "This is some text" CardHeading = "This is a featured cause card" CardImage = {image} />
                     <MoreI4cCard CardText = "This is some text" CardHeading = "This is a heading" CardImage = {image} />
                     <ButtonLink href="#" text={"link"} {...this.props} section={"The only section"}/>
                     <ButtonLink href="google.com" color={"green"} text="link2"/>
                     <ButtonLink href="react.com" color={"red"} text="link3"/>
-                    <MoreI4cCard CardText = "This is some text" CardHeading = "This is a heading" CardImage = "http://writingexercises.co.uk/images2/randomimage/pipe-sculpture.jpg" />
-                    <MoreI4cCard CardText = "This is some text" CardHeading = "This is a heading" CardImage = "http://writingexercises.co.uk/images2/randomimage/pipe-sculpture.jpg" />
-
-                    <MoreI4cCard CardText = "This is some text" CardHeading = "This is a heading" CardImage = "http://writingexercises.co.uk/images2/randomimage/pipe-sculpture.jpg" />
-                    <MoreI4cCard CardText = "This is some text" CardHeading = "This is a heading" CardImage = "http://writingexercises.co.uk/images2/randomimage/pipe-sculpture.jpg" />
-                    <MoreI4cCard CardText = "This is some text" CardHeading = "This is a heading" CardImage = "http://writingexercises.co.uk/images2/randomimage/pipe-sculpture.jpg" />
                     <MoreI4cCard CardText = "This is some text" CardHeading = "This is a heading" CardImage = "http://writingexercises.co.uk/images2/randomimage/pipe-sculpture.jpg" />
                     <MoreI4cCard CardText = "This is some text" CardHeading = "This is a heading" CardImage = "http://writingexercises.co.uk/images2/randomimage/pipe-sculpture.jpg" />
 
