087faf64e670babe3ef26bfbe1e87b022b48b6a3
David Ramsdale <davidramsdale1991@gmail.com>
2019-01-17 10:27:13 +1100

    Add margin in more i4c card

--- src/components/pages/HomePage/FeaturedCauseSection.js
+++ src/components/pages/HomePage/FeaturedCauseSection.js
@@ -3,13 +3,8 @@ import styled from 'styled-components';
 import image from "./../../../img/cartoon.jpg";
 import SectionWrapper from "./../HomePage/SectionWrapper";
 import FeaturedCauseCard from '../../modules/FeaturedCauseCard';
+import ButtonLink from './../../modules/ButtonLink';
 
-const FlexDiv = styled.div`
-    display: flex;
-    width: 80%;
-    height: auto;
-    justify-content: space-around;
-`
 const H = styled.h1`
     width:100%;
     text-align: center;
@@ -23,7 +18,7 @@ class FeacturedCauseSection extends Component {
                 <H>Featured Causes</H>
                 <FeaturedCauseCard CardHeading="This is a heading" CardText="This is some text" CardImage={image}  />
                 <FeaturedCauseCard CardHeading="This is a heading" CardText="This is some text" CardImage={image}  />    
-
+                <ButtonLink />
             </SectionWrapper>
         )
     }
--- src/components/pages/HomePage/MoreI4cSection.js
+++ src/components/pages/HomePage/MoreI4cSection.js
@@ -4,11 +4,6 @@ import image from "./../../../img/cartoon.jpg";
 import SectionWrapper from "./SectionWrapper";
 import MoreI4cCard from '../../modules/MoreI4cCard';
 
-const FlexDiv = styled.div`
-    display: flex;
-    width: 80%;
-    justify-content: space-around;
-`
 const H = styled.h1`
     color: white;
     width:100%;
@@ -21,11 +16,9 @@ class MoreI4cSection extends Component {
         return (
             <SectionWrapper color={color}>
                 <H>More from I4C</H>
-                <FlexDiv>
                     <MoreI4cCard CardHeading="This is a heading" CardText="This is some text" CardImage={image}  />
                     <MoreI4cCard CardHeading="This is a heading" CardText="This is some text" CardImage={image}  />
                     <MoreI4cCard CardHeading="This is a heading" CardText="This is some text" CardImage={image}  />
-                </FlexDiv>
 
             </SectionWrapper>
         )
