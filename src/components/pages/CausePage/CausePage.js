c1018426a8ecc27930729031358ce19e9f09a568
David Ramsdale <davidramsdale1991@gmail.com>
2019-01-22 09:34:53 +1100

    Add in filter for cause

--- src/components/pages/CausePage/CausePage.js
+++ src/components/pages/CausePage/CausePage.js
@@ -7,23 +7,25 @@ class CausePage extends Component {
     }
 
     render() {
-        console.log('test', this.props.cause)
+        console.log('test', this.props)
         // console.log('store', store.getState())
         const {color} = this.props;
+        const {slug} = this.props.match.params
+        const cause = this.props.cause.cause.filter(cause => cause.slug === slug)[0]
+        console.log("Cause: ", cause)
+        console.log("Slug", slug)
         return (
            <>
-               {this.props.cause.map(cause => {
-                        
-                    return(
-                        <h1>{cause.title}</h1>
-                    )
-                })}
+               <h1>{cause.pageContent.heading}</h1>
+
+               <p>{cause.pageContent.text}</p>
            </>
         )
     }
 }
 
 const mapStateToProps = () => {
+    //Add in an function to check if the state is in the store if not fire an action and using the slug to filter down.
     const {cause, causeIsFetching, causeError} = store.getState()
     return {
       cause,
