0fea92b7bbdeca4208da2582bca75e1f1643cfdb
David Ramsdale <davidramsdale1991@gmail.com>
2019-01-22 09:58:12 +1100

    Add about home page

---
+++ src/components/pages/AboutPage/AboutHomePage.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+import NavBar from '../../modules/NavBar';
+
+class AboutHomePage extends Component {
+    
+    componentDidMount() {
+        
+    }
+
+    render () {
+        return (
+            <div>
+                <NavBar />
+            </div>
+        );
+    }
+    
+}
+
+
+
+
+export default AboutHomePage;
