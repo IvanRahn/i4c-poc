b44baf78e761d61e0d9155adf643498239f4120b
Ivan <ivan.rahn@gmail.com>
2019-01-23 09:36:25 +1100

    change test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import App from './App';
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { shallow, mount } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({adapter: new EnzymeAdapter()})
 
 it('renders without crashing', () => {
-  shallow(<App />);
+  mount(<App />);
   
 });
 
