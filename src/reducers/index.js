f40a0df02fcbf623a1ca692ca5e868623c637187
David Ramsdale <davidramsdale1991@gmail.com>
2019-01-24 17:55:24 +1100

    Add reducers for about page to combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,11 +4,18 @@ import impact_reducer from "./impact_reducer";
 import cause_reducer from "./cause_reducer"; 
 import board_members_reducer from "./board_members_reducer";
 import aboutFirstSection from "./about_first_section";
+import aboutSecondSection from "./about_second_section";
+import aboutThirdSection from "./about_third_section";
+import aboutVolunteerSection from "./about_volunteer_section";
+
 
 export default combineReducers({
     causes: featuredCauseCardReducer,
     impact: impact_reducer,
     cause: cause_reducer,
     member: board_members_reducer,
-    about: aboutFirstSection,
+    aboutfirst: aboutFirstSection,
+    aboutsecond: aboutSecondSection,
+    aboutthird: aboutThirdSection,
+    aboutvolunteer: aboutVolunteerSection,
 });
