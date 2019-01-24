2979ed945959d486f75d930398a4a949392e90a7
Ivan <ivan.rahn@gmail.com>
2019-01-24 13:55:08 +1100

    add how it works

--- src/components/modules/HowItWorksCard.js
+++ src/components/modules/HowItWorksCard.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";  
 import styled from "styled-components";
-
+import VolunteerCard from './VolunteerCard';
 
 const Li = styled.li` 
   margin: 0 0 0.5rem 0;
@@ -8,34 +8,9 @@ const Li = styled.li`
   counter-increment: my-awesome-counter;
   position: relative;
 
-::before {
-    content: counter(my-awesome-counter);
-    color: white;
-    font-size: 2.5rem;
-    font-weight: bold;
-    position: absolute;
-    /* margin: auto; */
-    --size: 50px;
-    left: calc(-1 * var(--size) + 12px);
-    /* line-height: var(--size); */
-    width: var(--size);
-    height: var(--size);
-    top: 200px;
-    background: transparent;
-    border-radius: 50%;
-    text-align: center;
-    border-style: solid;
-}
 `
 
-const ImageContainer = styled.img`
-    display: flex;
-    flex-direction: column;
-    height: 150px;
-    width: 150px;
-    border-radius: 100%;
-    margin: auto;
-`
+
 
 
 class HowitWorksCard extends Component {
@@ -43,11 +18,7 @@ class HowitWorksCard extends Component {
         const {cardHeading, cardText, CardImage } = this.props
         return (
                 <Li>
-                    <div>
-                    <ImageContainer src= {CardImage} />
-                    <h1>{cardHeading}</h1> 
-                    <h3>{cardText}</h3>
-                    </div>
+                    <VolunteerCard CardHeading={cardHeading} CardText={cardText} CardImage={CardImage}/>
                 </Li>
                            
         );
--- src/components/pages/HomePage/HowItWorksSection.js
+++ src/components/pages/HomePage/HowItWorksSection.js
@@ -1,36 +1,39 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
 import image from "./../../../img/cartoon.jpg";
-import SectionWrapper from "./SectionWrapper";
-import HowItWorksCard from '../../modules/HowItWorksCard';
+import SectionWrapper from "./../../modules/SectionWrapperV2";
 import OrdList from '../../modules/HowItWorksList';
+import VolunteerCard from '../../modules/VolunteerCard';
 
 const H = styled.h1`
 width: 100%;
 text-align: center;
 margin-top: 120px;
 `
-class HowitWorksSection extends Component {
+class HowItWorksSection extends Component {
     render() {
         const {color} = this.props;
         return (
             <>
-            <SectionWrapper color={color}>
+            <SectionWrapper color={color} height="auto">
             <H>Donate once, give forever</H>
                 <OrdList>
-                <HowItWorksCard
-        cardHeading = "You Choose Your Cause" 
-        cardText = {
+                    <li>
+                <VolunteerCard
+        CardHeading = "You Choose Your Cause" 
+        CardText = {
         <div>
           <p> With over 50,000 charities in Australia, how do you make the right choice? </p> 
           <p> Our team of analysts determine the best charities that serve the cause of your choice. </p>
         </div>
         }
         CardImage = {image}
-                />           
-                <HowItWorksCard
-        cardHeading = "We Invest Responsibly" 
-        cardText = {
+                />    
+                </li>
+                <li>
+                <VolunteerCard
+        CardHeading = "We Invest Responsibly" 
+        CardText = {
         <div>
           <p> We invest the donations in Australian equities to generate significantly greater returns.  </p> 
           <p> This provides meaningful annual distributions to high impact charities that serve your cause. </p>
@@ -38,9 +41,11 @@ class HowitWorksSection extends Component {
         }
         CardImage = {image}
                 />     
-                <HowItWorksCard
-        cardHeading = "Charities Benefit Forever" 
-        cardText = {
+                </li>       
+                <li>
+                <VolunteerCard
+        CardHeading = "Charities Benefit Forever" 
+        CardText = {
         <div>
           <p> The impact of a single donation is only temporary. </p> 
           <p> I4C runs with zero operational costs and donations are grown through investments given to high impact charities every year into the future. </p>
@@ -48,6 +53,7 @@ class HowitWorksSection extends Component {
         }
         CardImage = {image}
                 />     
+                </li>
                 </OrdList>
             </SectionWrapper>
             </>
@@ -55,4 +61,4 @@ class HowitWorksSection extends Component {
     }
 }
 
-export default HowitWorksSection;
+export default HowItWorksSection;
