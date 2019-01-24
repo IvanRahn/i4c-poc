98eafefcc4523c4d19aabd693ee4e93f99908e49
David Ramsdale <davidramsdale1991@gmail.com>
2019-01-24 17:33:34 +1100

    Add to about first section to combine reducers

--- src/apis/keystone_api.js
+++ src/apis/keystone_api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const KeystoneApi = axios.create({
     baseURL: "https://i4c-cms.herokuapp.com"
+    
+    
 });
 
 export default KeystoneApi;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import featuredCauseCardReducer from "./featured_cause_cards_reducer";
 import impact_reducer from "./impact_reducer";
 import cause_reducer from "./cause_reducer"; 
 import board_members_reducer from "./board_members_reducer";
+import aboutFirstSection from "./about_first_section";
 
 export default combineReducers({
     causes: featuredCauseCardReducer,
     impact: impact_reducer,
     cause: cause_reducer,
-    member: board_members_reducer, 
+    member: board_members_reducer,
+    aboutFirstSection: aboutFirstSection,
 });
