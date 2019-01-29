17c17961bee0c6e6c12798178026dea3f7db2347
David Ramsdale <davidramsdale1991@gmail.com>
2019-01-29 11:32:12 +1100

    update import in about home page

--- src/components/pages/AboutPage/AboutHomePage.js
+++ src/components/pages/AboutPage/AboutHomePage.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import FirstSection from './sections/FirstSection';
 import SecondSection from './sections/SecondSection';
 import ThirdSection from './sections/ThirdSection';
-import CenteredContent from '../../modules/CenteredContent';
+import CenteredContent from '../../modules';
 import VolunteerSection from './sections/VolunteerSection'
 import withTracker from '../../google_analytics/withTracker';
 import { withRouter } from 'react-router-dom';
