43db584bfe5c29330cdb576fb39de98f860c08f7
David Ramsdale <davidramsdale1991@gmail.com>
2019-01-29 16:45:13 +1100

    Add donate section in cause page

---
+++ src/components/pages/CausePage/sections/DonateOnce.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import styled from 'styled-components';
+import {SectionWrapper} from "../../../modules";
+import { connect } from 'react-redux';
+import getContent from '../../../../actions/keystoneActions';
+import Card from '../../../modules/MoreI4cCard';
+
+const H = styled.h1`
+width: 100%;
+text-align: center;
+margin-top: 120px;
+`
+class DonateSection extends Component {
+    componentDidMount() {
+        this.props.getContent("HOWITWORKSSTEPS")
+    }
+    render() {
+
+        const {color, steps, stepsIsFetching, stepsError} = this.props;
+        if (stepsIsFetching) {
+            return <div>Loading</div>
+        } else if (stepsError || !steps || !steps[0]) {
+            return <div>error</div>
+        }
+        
+        return (
+            <>
+
+            <SectionWrapper id="HowItWorks" color={color} >
+                <H>Donate once, give forever</H>
+                <Card Cardheading="This is a heading" CardText="This is some text"/>
+            
+            </SectionWrapper>
+            </>
+        )
+    }
+}
+const mapStateToProps = (state) => {
+    const {steps, stepsIsFetching, stepsError} = state.steps
+   return {
+        steps: steps,
+        stepsIsFetching: stepsIsFetching,
+        stepsError: stepsError,
+   }
+}
+export default connect(mapStateToProps, {
+    getContent
+})(DonateSection);
