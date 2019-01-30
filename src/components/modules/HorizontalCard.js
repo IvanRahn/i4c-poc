0bab64ddfa3b1c514353b4cdb91d8469df9e0f3b
David Ramsdale <davidramsdale1991@gmail.com>
2019-01-30 11:43:41 +1100

    Update styling on horizontal card

--- src/components/modules/HorizontalCard.js
+++ src/components/modules/HorizontalCard.js
@@ -4,22 +4,43 @@ import {HTMLContent} from "./index"
 
 const CardWrapper = styled.div`
 display: flex;
+flex-direction: column;
 overflow: hidden;
 padding: 0 0 32px;
 margin: 48px auto 0;
 width: 100%;
 margin: 0px 24px;
+
+@media only screen and (min-width: 500px) {
+    display: flex;
+    flex-direction: row;
+    overflow: hidden;
+    padding: 0 0 32px;
+    margin: 48px auto 0;
+    width: 100%;
+    margin: 0px 24px;
+ }
 `
 const DivContainer = styled.div`
-width: 50%;
-padding: 2px 16px;
+width: 100%;
+padding: 2px 80px;
+
+@media only screen and (min-width: 500px) {
+    width: 50%;
+    padding: 2px 16px;
+ }
 `
 const DivImageContainer = styled.div`
-width: 50%;
+width: 100%;
 padding: 2px 16px;
 display: flex;
 justify-content: center;
 align-items: center;
+
+@media only screen and (min-width: 500px) {
+    width: 50%;
+    padding: 2px 16px;
+ }
 `
 
 const ImageContainer = styled.img`
@@ -34,18 +55,17 @@ class HorizontalCard extends Component {
         const {CardHeading, CardText, CardImage} = this.props;
 
         return(
-            <div>
-                <CardWrapper>
-                    <DivImageContainer>
-                        <ImageContainer src= "http://writingexercises.co.uk/images2/randomimage/sandy-boots.jpg" />
-                    </DivImageContainer>
-                    <DivContainer>
-                        <h2>Surname</h2>
-                        <h2>position</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    </DivContainer>
-                </CardWrapper>
-            </div>
+            <CardWrapper>
+                <DivImageContainer>
+                    <ImageContainer src= "http://writingexercises.co.uk/images2/randomimage/sandy-boots.jpg" />
+                </DivImageContainer>
+                <DivContainer>
+                    <h2>Surname</h2>
+                    <h2>position</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                </DivContainer>
+            </CardWrapper>
+
         )
     }
 }
