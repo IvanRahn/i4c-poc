6f59632d39777cabd6efaef42c0e45d503c6810f
Mike McCallum <mikemgmcc@gmail.com>
2019-01-31 15:06:59 +1100

    set up the HowItWorksHomePage and created an exact path for it

---
+++ src/components/pages/HowItWorksPage/HowItWorksHomePage.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class test extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <>
+            <h2>Hello</h2>
+            </>
+         );
+    }
+}
+ 
+export default test;
