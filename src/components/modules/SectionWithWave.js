4682b13cda035eb99277679256b7996ff5059ab1
Ivan <ivan.rahn@gmail.com>
2019-01-31 15:08:53 +1100

    adjust wave position

--- src/components/modules/SectionWithWave.js
+++ src/components/modules/SectionWithWave.js
@@ -1,27 +1,26 @@
-import React, {Component} from "react";
+import React from "react";
 import { SectionWrapper } from ".";
 import styled from "styled-components";
 import Background from "./../../img/placeholder_background_wave.png"
 const Img = styled.img `
     width: 100%;
     margin: auto auto 0;
-    /* position: absolute; */
-    /* top: -30px; */
+    position: absolute;
+    top: -30px;
     margin-bottom: -8px;
     @media only screen and (min-width: 500px) {
-    /* top: -108px; */
-    /* margin-bottom: -200px; */
+    bottom: calc(100% + 8px);
     }
 `
 const SectionWithWave = ({height, id, color, children}) => {
         return (     
             <>
-            <Img src={Background} alt =""/>
             <SectionWrapper 
             height={height}
             id={id}
             color={color}
             >
+            <Img src={Background} alt =""/>
 
                 {children}
 
