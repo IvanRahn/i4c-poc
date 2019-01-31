f6b0e7318b33b6b7241b5a68cf5e66d72b496cc3
Ivan <ivan.rahn@gmail.com>
2019-01-31 15:16:05 +1100

    separate section

--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -6,8 +6,8 @@ import {darkGreen} from "./../../modules/BrandStyle"
 const HowItWorksSection = lazy(() => import('./../../modules/HowItWorksSection')) ;
 console.log('TCL: HowitWorksSection', HowItWorksSection)
 const MoreI4cSection = lazy(() => import('./sections/MoreI4cSection')) ;
-const FeaturedCauseSection = lazy(() => import('./sections/FeaturedCauseSection')) ;
-const TeamSection= lazy(() => import('./../../modules/FeaturedCauseSection')) ;
+const FeaturedCauseSection = lazy(() => import('./../../modules/FeaturedCauseSection')) ;
+const TeamSection= lazy(() => import('./sections/TeamSection')) ;
 
 
 const HomePage = () => {
