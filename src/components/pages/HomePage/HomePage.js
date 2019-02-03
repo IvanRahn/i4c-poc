d895d9c25fb289b8fea863e96e332acc975fbb9f
Ivan <ivan.rahn@gmail.com>
2019-02-04 10:51:26 +1100

    lazy imports

--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -2,9 +2,9 @@ import React, { Suspense, lazy } from "react";
 import ImpactSection from './sections/ImpactSection';
 import withTracker from '../../google_analytics/withTracker';
 import {darkGreen} from "./../../modules/BrandStyle"
-const HowItWorksSection = lazy(() => import('./../../modules')) ;
+const HowItWorksSection = lazy(() => import('./../../modules/pageLayout/HowItWorksSection')) ;
 const MoreI4cSection = lazy(() => import('./sections/MoreI4cSection')) ;
-const FeaturedCauseSection = lazy(() => import('./../../modules')) ;
+const FeaturedCauseSection = lazy(() => import('./../../modules/pageLayout/FeaturedCauseSection')) ;
 const TeamSection= lazy(() => import('./sections/TeamSection')) ;
 
 
