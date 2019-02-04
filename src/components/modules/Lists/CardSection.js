5d77b75ea9ab09627ff3805569eea7082b48efdc
Mike McCallum <mikemgmcc@gmail.com>
2019-02-05 10:57:46 +1100

    fixed dev problem

--- src/components/modules/Lists/CardSection.js
+++ src/components/modules/Lists/CardSection.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
-import {SmallHorizontalCard} from '..';
+import {SmallHorizontalCard, ImpactStatsCard} from '..';
 
 
 const Wrapper = styled.div`
