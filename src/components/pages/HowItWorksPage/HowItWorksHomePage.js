1aeca03fc7f561d21ba528adf3dd4f664f29a7eb
Mike McCallum <mikemgmcc@gmail.com>
2019-02-05 10:08:14 +1100

    changed an import component in how it works home page

--- src/components/pages/HowItWorksPage/HowItWorksHomePage.js
+++ src/components/pages/HowItWorksPage/HowItWorksHomePage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PageOpener from '../../modules/pageLayout/PageOpener'; 
-import {SmallHorizontalCard, FeaturedCauseSection, HowItWorksSection, HeroArticleCard} 
+import {FeaturedCauseSection, HowItWorksSection, HeroArticleCard} 
 from './../../modules'; 
 import {darkGreen} from "./../../modules/BrandStyle"
 import {connect} from "react-redux"; 
@@ -32,9 +32,14 @@ class HowItWorksHomePage extends Component {
                     heading="Blah"
                     text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." 
                     image={image}  
-                    link="yo" 
+                    breadcrumbs={[ 
+                        {to: "/", text: "OUR APPROACH"}
+                    ]} 
                     verticalCardApi= "howitworkshomepagecard"
-                    verticalCardState="howItWorksHomePageCard" /> 
+                    verticalCardState="howItWorksHomePageCard" > 
+                    <>
+                    </>
+                    </PageOpener> 
 
 
                     <HowItWorksSection cardCount={6} color={darkGreen}/>
